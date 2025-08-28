256609ee741c26aa2bd06dd9d0bf9e156e59a59d
Heather Maedl <hmaedl@cisco.com>
2025-08-28 14:53:07 -0700

    initial commit: add SwiftUI practice project

---
+++ Practice App/Practice App/Practice_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_AppApp.swift
+//  Practice App
+//
+//  Created by hmaedl on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Practice_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Practice App/Practice AppTests/Practice_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_AppTests.swift
+//  Practice AppTests
+//
+//  Created by hmaedl on 8/28/25.
+//
+
+import Testing
+@testable import Practice_App
+
+struct Practice_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
